d3ed6f99f76e1ee44c29515763981d1067930638
per0za <perozaerick@protonmail.com>
2024-03-22 20:57:21 -0300

    Created a delete route the Seller model

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Identity.Client;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 
@@ -22,6 +24,18 @@ namespace SalesWebMVC.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Sellers.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Sellers.Find(id);
+            _context.Sellers.Remove(obj);
             _context.SaveChanges();
         }
     }
